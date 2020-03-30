4101c84f406ccbe8911d0b30794ed1db5904d317
unknown <achref.gomri@gmail.com>
2020-03-30 18:39:11 +0100

    error

--- Zcasino.py
+++ Zcasino.py
@@ -3,28 +3,53 @@ from os import system, name
 #print("Bonjour joueur et bienvenue au Zcasino! ton solde initial est de 500$")
 #solde=int(500)
 #montant=''
-#numero_mise=''
+#numero_mise
 i=0
-while(True):
+def clear(): 
+  
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+  
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear') 
+def initialiser(solde):
+    clear()
+    print("Bonjour joueur et bienvenue au Zcasino! ton solde initial est de {}$".format(solde))
+    montant=''
+    numero_mise=''
+    i=1
+
+
+def transformer():
     i+=1
-    print("c'est le tour numéro: {}".format(i))
+def aide_afficher(i):
+    i+=1
     while(True):
         try:
             montant=input("veuiller saisir le montant à miser, q pour quiter")
             montant=int(montant)
+            clear()
+            afficher()
             numero_mise=input("saisissez le numero de la mise, q pour quiter")
             numero_mise=int(numero_mise)
+            clear()
+            afficher()
             if(not (montant < 1 or montant > solde or numero_mise<0 or numero_mise>49)):
                 solde=solde-montant
                 break
             else:
+                clear()
+                afficher()
                 print("le numero est erronée,veuiller reessayer")
         except:
             print("le numero est erronée,veuiller reessayer")
             if montant=='q' or numero_mise=='q':
                 break
+        
     if montant=='q' or numero_mise=='q':
-        break
+        return ''
     numGagnant=random.randrange(50)
     if numGagnant==numero_mise :
         solde=solde+(montant*3)
@@ -35,20 +60,16 @@ while(True):
     else:
         print ("dommage que vous avez rien gagné, le numéro gagnant est le {}, solde restant est de {} en revenche vous pouvez toujours reesseyer".format(numGagnant,solde))
     print("Votre solde est de {}$, q pour quiter".format(solde))
-print("au revoir et à trés bientôt")        
-def clear(): 
-  
-    # for windows 
-    if name == 'nt': 
-        _ = system('cls') 
-  
-    # for mac and linux(here, os.name is 'posix') 
-    else: 
-        _ = system('clear') 
-def initialiser():
-    print("Bonjour joueur et bienvenue au Zcasino! ton solde initial est de 500$")
-    solde=int(500)
-    montant=''
-    numero_mise=''
-def transformer():
     
+def afficher():
+    print("c'est le tour numéro: {} \n \n".format(i))
+    
+    
+initialiser(500)
+while(True):
+#    transformer()
+    afficher()
+    aide_afficher(i)
+    if montant=='q' or numero_mise=='q':
+        break
+print("au revoir et à trés bientôt")
