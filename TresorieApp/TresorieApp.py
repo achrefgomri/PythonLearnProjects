f1f4e42d8093034d775926a09de8969278f6ab6b
achref <achref.gomri@gmail.com>
2020-04-08 11:24:41 +0100

    .gitignore TresorieAPP lib/

---
+++ TresorieApp/TresorieApp.py
@@ -0,0 +1,10 @@
+from lib import TresorieClass
+
+
+
+def main():
+	
+	print("c'est le proramme principale")
+
+if __name__=='__main__':
+	main()
---
+++ TresorieApp/lib/TresorieClass.py
@@ -0,0 +1,26 @@
+class Caisse:
+	
+	def __init__(self,ID,nom,valeur):
+		self.ID=ID
+		self.nom=nom
+		self.valeur=valeur
+	
+class Mouvement:
+	
+	def __init__(self,ID,ID_caisse,nature,valeur,motif,date):
+		self.ID=ID
+		self.ID_caisse=ID_caisse
+		self.nature=nature
+		self.valeur=valeur
+		self.motif=motif
+		self.date=date
+		
+	
+
+
+def main():
+	
+	return
+
+if __name__=='__main__':
+	main()
